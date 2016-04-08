8990b9bcc2dafa6e0d5facf354cbc44fe5fcf25b
IgorMedved <igor.medvedyev@gmail.com>
2016-04-08 04:16:05 -0400

    Version 0.78 using Timer

--- src/javaswing_applet/Main.java
+++ src/javaswing_applet/Main.java
@@ -1,12 +1,17 @@
 package javaswing_applet;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JApplet;
+import javax.swing.Timer;
 
 
-public class Main extends JApplet {
+public class Main extends JApplet implements ActionListener {
 
+	private Timer timer;
+	
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
@@ -15,6 +20,9 @@ public class Main extends JApplet {
 
 	@Override
 	public void init() {
+		
+		timer = new Timer (500,  this);
+		timer.start();
 		
 		setSize(600,500);
 		setLayout(new BorderLayout());
@@ -27,12 +35,19 @@ public class Main extends JApplet {
 	public void start() {
 		// TODO Auto-generated method stub
 		System.out.println("start");
+		timer.start();
 	}
 
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
 		System.out.println("stop");
+		timer.stop();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Timer is running");
 	}
 
 }
